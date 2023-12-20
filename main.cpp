da72461c4fe15431cbb0e99a5a99c0d769bd566b
VitoS <ikn0@proton.me>
2023-12-20 10:21:17 -0800

    first comit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Einlesen Auslesen
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+    string vorname;
+};
+
+int main()
+{
+
+}
